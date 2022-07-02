3b5def61a33e493bed5d57630678e54bba241f20
eliel <eliel123escobar@gmail.com>
2022-07-02 15:37:03 -0600

    ADD: Ruta de inicio

--- src/Services/GetWeather.jsx
+++ src/Services/GetWeather.jsx
@@ -1,6 +1,6 @@
 const apiKey = 'c8e4b2b68d514f0c8e7220512220107'
 
-export default function GetWeather({ keyword }) {
+export default function GetWeather({ keyword = 'Managua' }) {
   const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${keyword}&aqi=no`
 
   return fetch(apiUrl)
