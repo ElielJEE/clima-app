820b38ab2d0d13597a072a442fc81b709554c5a7
eliel <eliel123escobar@gmail.com>
2022-07-02 15:03:19 -0600

    FIX: URL de la api se cambio de http a https

--- src/Services/GetCities.jsx
+++ src/Services/GetCities.jsx
@@ -1,7 +1,7 @@
 const apiKey = 'c8e4b2b68d514f0c8e7220512220107'
 
 export default function GetCities({ city }) {
-  const urlApi = `http://api.weatherapi.com/v1/search.json?key=${apiKey}&q=${city}`
+  const urlApi = `https://api.weatherapi.com/v1/search.json?key=${apiKey}&q=${city}`
 
   return fetch(urlApi)
     .then(response => response.json())
--- src/Services/GetWeather.jsx
+++ src/Services/GetWeather.jsx
@@ -1,7 +1,7 @@
 const apiKey = 'c8e4b2b68d514f0c8e7220512220107'
 
 export default function GetWeather({ keyword }) {
-  const apiUrl = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${keyword}&aqi=no`
+  const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${keyword}&aqi=no`
 
   return fetch(apiUrl)
     .then(response => response.json())
